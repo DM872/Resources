9c05364ade218e7ce28e4a6d7ca7196089f7591e
kpavlikov <pavlikoff.ko@gmail.com>
2026-02-19 13:41:47 +0100

    Create main_ATSP_template.py

---
+++ units/02/code/main_ATSP_template.py
@@ -0,0 +1,101 @@
+#-----------------------------------------------------------------------------------------------------------------------
+from TSP_utilities import *
+from gurobipy import *
+#-----------------------------------------------------------------------------------------------------------------------
+def find_subtours(x_values):
+    G = nx.Graph()
+    for (i, j) in x_values.keys():
+        if x_values[i, j] > 0.5:
+            G.add_edge(i, j)
+    returned_subtours = []
+    for tour in nx.connected_components(G):
+        returned_subtours.append(tour)
+    return returned_subtours
+#-----------------------------------------------------------------------------------------------------------------------
+def ATSP(folder, filename, Asymmetry_Flag, data_format_flag, n, relax, time_lim):
+    d = load_instance(folder, filename, n, Asymmetry_Flag, data_format_flag)
+    #-------------------------------------------------------------------------------------------------------------------
+    def subtour_elimination_lazy_callback(model, where):
+        if where == GRB.Callback.MIPSOL:
+            x_values = model.cbGetSolution(model._x)
+            subtours = find_subtours(x_values)
+            if len(subtours) > 1:
+                for S in subtours:
+                    if len(S) <= model._n / 2:
+                        model.cbLazy(quicksum(model._x[i, j] for i in S for j in S if i != j) <= len(S) - 1)
+                    else:
+                        S_bar = set(range(0, model._n)).difference(S)
+                        model.cbLazy(quicksum(model._x[i, j] for i in S_bar for j in S_bar if i != j) <= len(S_bar) - 1)
+    #-------------------------------------------------------------------------------------------------------------------
+    def DFJ_callback(model, where):
+        if where == GRB.Callback.MIPNODE:
+            # add cuts at every bnb node
+            if model.cbGet(GRB.callback.MIPNODE_NODCNT) % 3 == 0:
+                if model.cbGet(GRB.Callback.MIPNODE_STATUS) == GRB.OPTIMAL:
+                    x_values = model.cbGetNodeRel(model._x)
+
+                    model.cbCut(0, GRB.LESS_EQUAL, 0)
+    #-------------------------------------------------------------------------------------------------------------------
+    def complete_callback(model, where):
+        #DFJ_callback(model, where)
+        #subtour_elimination_lazy_callback(model, where)
+    #-------------------------------------------------------------------------------------------------------------------
+    m = Model("Optimize")
+    x = {}
+    for i in range(0, n):
+        for j in range(0, n):
+            if i != j:
+                x[i, j] = m.addVar(vtype=GRB.CONTINUOUS, name='x_' + str(i) + '_' + str(j))
+    for i in range(0, n):
+        m.addLConstr(quicksum(x[i, j] for j in range(0, n) if i != j), GRB.EQUAL, 1)
+        m.addLConstr(quicksum(x[j, i] for j in range(0, n) if i != j), GRB.EQUAL, 1)
+
+    obj = quicksum(d[i, j] * x[i, j] for i in range(0, n) for j in range(0, n) if i != j)
+    #m.setParam('OutputFlag', False)
+    m.setObjective(obj, GRB.MINIMIZE)
+    # DL
+    # GG m.
+    add_root_node_DFJ_cuts = True
+    if add_root_node_DFJ_cuts:
+        m.optimize()
+        x_values = {(i, j): x[i, j].x for (i, j) in x.keys()}
+        #print(x_values[(0, 1)])
+        # ....
+        add_root_node_DFJ_cuts = False
+
+    if not relax:
+        # set up the MILP problem
+        for (i, j) in x.keys():
+            x[i, j].vtype = GRB.BINARY
+        # provide the simplest 1 -> 2 -> 3 .... n -> 1 to the solver as a starting solution
+        for k in range(1, n):
+            x[k - 1, k].start = 1.0
+        m.Params.LazyConstraints = 1
+        m._x = x
+        m._n = n
+        m._epsilon_1 = pow(10, -4)
+        m._epsilon_2 = pow(10, -3)
+        m.setParam('MIPGap', 0) # means that an optimal solution is being searched for
+        m.setParam('MIPGapAbs', 0.99) # means that the absolute gap might be up to 1 withoug violating optimality conditions
+        m.setParam('Timelimit', time_lim)
+        # no callback
+        #m.optimize()
+        # with callback
+        m.optimize(complete_callback)
+#-----------------------------------------------------------------------------------------------------------------------
+def main_ATSP():
+    relax = False
+    Asymmetry_Flag = True
+    time_lim = 3600
+    #ATSP_DFJ('realfiles', 'dc112', Asymmetry_Flag, 'VECTOR', 112, relax, time_lim)
+    ATSP_DFJ('realfiles', 'dc126', Asymmetry_Flag, 'VECTOR', 126, relax, time_lim)
+    #ATSP_DFJ('realfiles', 'dc134', Asymmetry_Flag, 'VECTOR', 134, relax, time_lim)
+    #ATSP_DFJ('realfiles', 'dc176', Asymmetry_Flag, 'VECTOR', 176, relax, time_lim)
+    #ATSP_DFJ('realfiles', 'dc188', Asymmetry_Flag, 'VECTOR', 188, relax, time_lim)
+    #ATSP_DFJ('realfiles', 'dc563', Asymmetry_Flag, 'VECTOR', 563, relax, time_lim)
+    #ATSP_DFJ('realfiles', 'dc849', Asymmetry_Flag, 'VECTOR', 849, relax, time_lim)
+    #ATSP_DFJ('realfiles', 'dc895', Asymmetry_Flag, 'VECTOR', 895, relax, time_lim)
+    #ATSP_DFJ('realfiles', 'dc932', Asymmetry_Flag, 'VECTOR', 932, relax, time_lim)
+#-----------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    main_ATSP()
